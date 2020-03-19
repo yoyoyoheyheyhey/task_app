584e0f058ea27a0f7d7646d263b6579a9f46e39d
yodev21 <yodev2177@gmail.com>
2020-03-19 15:11:32 +0900

    タイムゾーンを日本に設定

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,7 @@ module TaskApp
                        controller_specs: false,
                        request_specs: false
     end
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
