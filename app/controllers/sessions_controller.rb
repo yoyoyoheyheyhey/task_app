4b79a70850b2348914a76c6c03d8e507af62470c
yodev21 <yodev2177@gmail.com>
2020-03-27 18:04:07 +0900

    ログイン画面を実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  def new
+  end
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
