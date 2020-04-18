4f5a25225fdca447e02ce175656c8e9fddd67b75
yodev21 <yodev2177@gmail.com>
2020-04-18 22:56:54 +0900

    carrierwave編集

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -16,9 +16,9 @@ CarrierWave.configure do |config|
   case Rails.env
     when 'production'
       config.fog_directory = '本番環境用のバケット名'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/本番環境用のバケット名'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/task-app-production'
     when 'development'
       config.fog_directory = '開発環境用のバケット名'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/開発環境用のバケット名'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/task-app-development'
   end
 end
