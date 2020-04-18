e8ab9c1fffa5d291790babfaa76c20c72470bc92
yodev21 <yodev2177@gmail.com>
2020-04-19 00:37:17 +0900

    production修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,14 +1,14 @@
-# server '18.178.57.8', user: 'app', roles: %w{app db web}
-#set :ssh_options, keys: '/Users/yotanaka/.ssh/id_rsa'
+server '18.178.57.8', user: 'app', roles: %w{app db web}
+set :ssh_options, keys: '/Users/yotanaka/.ssh/id_rsa'
 
-server '18.178.57.8',
-   user: "app",
-   roles: %w{web db app},
-   ssh_options: {
-       #port: 22022,
-       #user: "odatakashi", # overrides user setting above
-       keys: '/Users/yotanaka/.ssh/id_rsa',
-       forward_agent: true
+# server '18.178.57.8',
+#    user: "app",
+#    roles: %w{web db app},
+#    ssh_options: {
+#        #port: 22022,
+#        #user: "odatakashi", # overrides user setting above
+#        keys: '/Users/yotanaka/.ssh/id_rsa',
+#        forward_agent: true
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
-   }
+  #  }
