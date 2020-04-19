a9540dbbf4ac136f56ce81e47618dfaff6c0b97c
yodev21 <yodev2177@gmail.com>
2020-04-19 20:18:43 +0900

    scss

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   config.assets.compile = true
   config.assets.css_compressor = :sass
   config.assets.enabled = true
-  config.assets.precompile += %w(*.js *.less *.css *.erb)
+  config.assets.precompile += %w(*.js *.less *.css *.erb *.cscc)
   # config.serve_static_assets = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
