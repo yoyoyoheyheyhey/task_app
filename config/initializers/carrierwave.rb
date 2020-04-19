72dbcef90675d5fb309fabe84eaf4d5365d46599
yodev21 <yodev2177@gmail.com>
2020-04-19 15:58:11 +0900

    cache削除

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -15,10 +15,10 @@ CarrierWave.configure do |config|
   config.fog_attributes = {'Cache-Control' => 'public, max-age=86400'}
   case Rails.env
     when 'production'
-      config.fog_directory = '本番環境用のバケット名'
+      config.fog_directory = 'task-app-production'
       config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/task-app-production'
     when 'development'
-      config.fog_directory = '開発環境用のバケット名'
+      config.fog_directory = 'task-app-development'
       config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/task-app-development'
   end
 end
