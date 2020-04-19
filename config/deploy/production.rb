618482f1d415ca7935fd48f4da187c929017b78e
yodev21 <yodev2177@gmail.com>
2020-04-19 15:19:41 +0900

    Capfile修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,8 @@
 server '18.178.57.8', user: 'app', roles: %w{app db web}
 set :ssh_options, keys: '/Users/yotanaka/.ssh/id_rsa'
 
+set :assets_roles, [:web, :app]
+#server '18.178.57.8', roles: %w(web app)
 # server '18.178.57.8',
 #    user: "app",
 #    roles: %w{web db app},
