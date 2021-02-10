54b265f7356d009988c120b38cd60579bc3faf27
Yohei Goto <yohei_goto@diveintocode.jp>
2021-02-10 18:59:20 +0900

    :banana: statusとpriorityもランダムにする

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,8 +21,8 @@ end
       title: "test Title#{task + 1}",
       content: "test Content#{task + 1}",
       end_date: DateTime.current + [*-3..7].sample,
-      status: "未着手",
-      priority: 0,
+      status: %w[未着手 着手中 完了].sample,
+      priority: [0, 1, 2].sample,
       user_id: user.id
     )
     Labelling.create(task_id: task.id, label_id: Label.all.sample.id)
